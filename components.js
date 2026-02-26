5637e59a4dc02bf4f1234c6585e742ffdcf93835
choobipanda <atn26@humboldt.edu>
2026-02-26 11:49:34 -0800

    add css, turn footer and navbar into components

---
+++ components.js
@@ -0,0 +1,10 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+loadComponent("navbar-container", "navbar.html");
+loadComponent("footer-container", "footer.html");
